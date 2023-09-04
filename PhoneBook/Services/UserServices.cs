9ac1fa228ac3b55d6d6427be21a892658f1b1076
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 12:08:26 +0330

    addd User Service

---
+++ PhoneBook/Services/UserServices.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserServices
+    {
+        private UnitOfWork db = new UnitOfWork();
+        public response signUp(string name, string email, string password)
+        {
+            //تکراری نبودن
+            //autentication
+            db.UserRepository.AddUser(new User()
+            {
+                email = email,
+                password = password,
+                name = name
+            });
+            return new response() { };
+        }
+        public response logIn(string email, string password)
+        {
+            //پیدا کردن با ایمیل
+            //چک کردن رمز
+            return new response() { };
+        }
+    }
+}
