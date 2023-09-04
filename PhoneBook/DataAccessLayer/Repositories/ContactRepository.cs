f68c2a0359755733968e42199061268f15cf6042
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 09:53:27 +0330

    add Contact repository interface

---
+++ PhoneBook/DataAccessLayer/Repositories/ContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IContactRepository
+    {
+        bool AddContact(Contact contact);
+        List<Contact> GetAllContacts(int user_Id);
+        bool UpdateContact(Contact contact);
+        bool DeleteContact(int contactId);
+    }
+}
