cc4a3408c8a5e38a5605735648dace0a82c06bbc
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 07:41:10 +0330

    add contact model

---
+++ PhoneBook/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phone_number { get; set; }
+        public int user_Id { get; set; }
+    }
+}
