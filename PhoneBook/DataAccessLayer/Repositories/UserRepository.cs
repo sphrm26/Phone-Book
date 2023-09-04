5278c94d6dd86a9cbf5289a782879c7a29659534
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 07:48:34 +0330

    add user repository interface

---
+++ PhoneBook/DataAccessLayer/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IUserRepository
+    {
+        bool AddUser(User user);
+        User FindUserByEmail(string email);
+        bool UpdateUser(User user);
+        bool DeleteUser(int user_Id);
+    }
+}
