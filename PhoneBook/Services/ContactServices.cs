e414611c87c97b854e6d57369b2beb3b160d39ed
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 13:33:39 +0330

    add add contact method in contactservices

--- PhoneBook/Services/ContactServices.cs
+++ PhoneBook/Services/ContactServices.cs
@@ -19,5 +19,20 @@ namespace Services
                 isSuccess = true
             };
         }
+        public Response AddContact(int User_Id, string first_name, string last_name, string phone_number)
+        {
+            db.ContactRepository.AddContact(new Contact()
+            {
+                first_name = first_name,
+                last_name = last_name,
+                phone_number = phone_number,
+                user_Id = User_Id
+            });
+            return new Response()
+            {
+                isSuccess = true,
+                message = "new contact successfuly added"                
+            };
+        }
     }
 }
