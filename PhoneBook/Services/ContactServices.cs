84295a6a6ce12c81df6a6b66ed5cfc2f6ff95af1
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 13:30:45 +0330

    add get all contacts

---
+++ PhoneBook/Services/ContactServices.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ContactServices
+    {
+        private UnitOfWork db = new UnitOfWork();
+        public Response GetAllContact(int User_Id)
+        {
+            return new Response()
+            {
+                objects = db.ContactRepository.GetAllContacts(User_Id).ToList<Object>(),
+                isSuccess = true
+            };
+        }
+    }
+}
