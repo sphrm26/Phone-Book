f522a9b7403b18ca0c28f443f2834e0816f5d843
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 10:43:30 +0330

    add unit of work in data access layer

---
+++ PhoneBook/DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class UnitOfWork
+    {
+        string connectionString = @"Data Source = DESKTOP-7HLK92Q\SEPEHRSQL;Initial Catalog=PhoneBook;User ID=sa;Password=sphrm26sphrm26;";
+
+        private IUserRepository _userRepository;
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (_userRepository == null)
+                {
+                    _userRepository = new UserRepository(connectionString);
+                }
+
+                return _userRepository;
+            }
+        }
+
+        private IContactRepository _contactRepository;
+        public IContactRepository ContactRepository
+        {
+            get
+            {
+                if (_contactRepository == null)
+                {
+                    _contactRepository = new ContactRepository(connectionString);
+                }
+
+                return _contactRepository;
+            }
+        }
+    }
+}
