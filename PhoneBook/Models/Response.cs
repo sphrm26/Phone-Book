bbb21a0fcddf4ae819a2707a8e1024d0aca2b031
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-04 12:16:33 +0330

    add response model

---
+++ PhoneBook/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Response
+    {
+        public List<Object> objects { get; set; }
+        public bool isSuccess { get; set; }
+        public string message { get; set; }
+    }
+}
