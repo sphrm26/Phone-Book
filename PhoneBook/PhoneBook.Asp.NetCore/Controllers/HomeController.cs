904a7caafa31b2e0328b406c512e9fa6e4d3cfe5
SEPEHR <sphrm26sphrm26@gmail.com>
2023-09-06 17:14:38 +0330

    add Login page

--- PhoneBook/PhoneBook.Asp.NetCore/Controllers/HomeController.cs
+++ PhoneBook/PhoneBook.Asp.NetCore/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Models;
 
 namespace PhoneBook.Asp.NetCore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Enter(string Id = "SingUp")
         {
-            return View();
+            if(Id == "Login")
+            {
+                return View("Login");
+            }
+            return View("SignUp");
+        }
+        public Object SignUp(string email, string name, string password)
+        {
+            UserServices userService = new UserServices();
+            if (userService.SignUp(name, email, password).isSuccess)
+            {
+                
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- PhoneBook/PhoneBook.Asp.NetCore/Program.cs
+++ PhoneBook/PhoneBook.Asp.NetCore/Program.cs
@@ -18,6 +18,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Enter}/{id?}");
 
 app.Run();
